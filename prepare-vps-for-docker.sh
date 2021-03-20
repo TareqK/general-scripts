69db38008c39019b4b45f4c9bf23824496b313a3
Tareq Kirresh <kirreshtareq@gmail.com>
2021-03-20 17:19:22 +0200

    Update prepare-vps-for-docker.sh

--- prepare-vps-for-docker.sh
+++ prepare-vps-for-docker.sh
@@ -2,16 +2,12 @@
 touch /.dockerenv
 apt purge -f -y apache2
 apt update && apt upgrade -f -y
-apt install docker.io docker-compose docker ufw openjdk-11-jre cockpit cockpit-docker -f -y
+apt install docker.io docker-compose docker ufw openjdk-8-jre  -f -y
 ufw default allow outgoing
 ufw default deny incoming
 ufw allow http
 ufw allow https
 ufw allow ssh
-ufw allow 9090
-ufw allow 2377
-ufw allow 7946
-ufw allow 4789
 ufw --force enable 
 systemctl enable ufw
 reboot
